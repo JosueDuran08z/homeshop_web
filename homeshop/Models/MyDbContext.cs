a1f5efb147aee7cfcfce4185e50e677e0be31942
josueduran08z <josueduran08z@gmail.com>
2022-07-13 13:58:31 -0500

    Add project files.

---
+++ homeshop/Models/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace homeshop.Models
+{
+    public class MyDbContext:DbContext
+    {
+        private const string ConnectionString = "DefaultConnection";
+        public MyDbContext(): base(ConnectionString) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
